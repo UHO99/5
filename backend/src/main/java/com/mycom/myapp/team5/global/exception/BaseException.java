4414b55206833b0af020e9c17098906294495a9f
ddb27 <ddb2785@gmail.com>
2026-08-10 11:35:44 +0900

    Init Spring Server

--- backend/src/main/java/com/mycom/myapp/team5/global/exception/BaseException.java
+++ backend/src/main/java/com/mycom/myapp/team5/global/exception/BaseException.java
@@ -1,12 +1,13 @@
 package com.mycom.myapp.team5.global.exception;
 
 import com.mycom.myapp.team5.global.common.enums.CommonErrorCode;
+import com.mycom.myapp.team5.global.common.enums.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public abstract class BaseException extends RuntimeException {
 
-    private final CommonErrorCode errorCode;
+    private final ErrorCode errorCode;
 
     protected BaseException(CommonErrorCode errorCode) {
         super(errorCode.getMessage());
